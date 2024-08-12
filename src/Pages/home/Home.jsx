eb03ee14110c6691d138ffe162cfbb39404a1c1e
mohsinkhan60 <mohsinsaffy@gmail.com>
2024-08-12 14:09:40 +0500

    Fix deployment Error

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -2,8 +2,8 @@ import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
 import { userData } from "../../dummyData";
-import WidgetSm from "../../components/WidgetSm/widgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
 
 export const Home = () => {
   return (
