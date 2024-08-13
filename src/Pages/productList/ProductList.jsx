2747328ef3517e084ef0c8c0dc3ced80d2dcd89b
mohsinkhan60 <mohsinsaffy@gmail.com>
2024-08-13 13:10:21 +0500

    add complete Users Page

---
+++ src/Pages/productList/ProductList.jsx
@@ -0,0 +1,8 @@
+import "./productList.css"
+
+export const ProductList = () => {
+  return (
+    <div>ProductList</div>
+  )
+}
+export default ProductList
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,98 +1,103 @@
-import "./sidebar.css"
-import LineStyleIcon from '@mui/icons-material/LineStyle';
-import TimelineIcon from '@mui/icons-material/Timeline';
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
-import PermIdentityIcon from '@mui/icons-material/PermIdentity';
-import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import StackedBarChartIcon from '@mui/icons-material/StackedBarChart';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import PortraitIcon from '@mui/icons-material/Portrait';
-import ReportIcon from '@mui/icons-material/Report';
+import "./sidebar.css";
+import LineStyleIcon from "@mui/icons-material/LineStyle";
+import TimelineIcon from "@mui/icons-material/Timeline";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import StackedBarChartIcon from "@mui/icons-material/StackedBarChart";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import DynamicFeedIcon from "@mui/icons-material/DynamicFeed";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import PortraitIcon from "@mui/icons-material/Portrait";
+import ReportIcon from "@mui/icons-material/Report";
+import { NavLink } from "react-router-dom";
 
 export const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
-         <div className="sidebarMenu">
-            <h3 className="sidebarTitle">Dashboard</h3>
-            <ul className="sidebarList">
-               <li className="sidebarListItem active">
-               <LineStyleIcon className="sidebarIcon" />
-               Home
-               </li>
-               <li className="sidebarListItem">
-               <TimelineIcon className="sidebarIcon" />
-               Analytics
-               </li>
-               <li className="sidebarListItem">
-               <TrendingUpIcon className="sidebarIcon" />
-               Sales
-               </li>
-            </ul>
-         </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyleIcon className="sidebarIcon" />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <TimelineIcon className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUpIcon className="sidebarIcon" />
+              Sales
+            </li>
+          </ul>
+        </div>
 
-         <div className="sidebarMenu">
-            <h3 className="sidebarTitle">Quick Menu</h3>
-            <ul className="sidebarList">
-               <li className="sidebarListItem">
-               <PermIdentityIcon className="sidebarIcon" />
-               User
-               </li>
-               <li className="sidebarListItem">
-               <LocalGroceryStoreIcon className="sidebarIcon" />
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <NavLink to="users" className="link">
+              <li className="sidebarListItem">
+                <PermIdentityIcon className="sidebarIcon" />
+                User
+              </li>
+            </NavLink>
+            <NavLink to="products" className="link">
+              <li className="sidebarListItem">
+                <LocalGroceryStoreIcon className="sidebarIcon" />
                 Products
-               </li>
-               <li className="sidebarListItem">
-               <AttachMoneyIcon className="sidebarIcon" />
-                Transactions
-               </li>
-               <li className="sidebarListItem">
-               <StackedBarChartIcon className="sidebarIcon" />
-                Reports
-               </li>
-            </ul>
-         </div>
+              </li>
+            </NavLink>
+            <li className="sidebarListItem">
+              <AttachMoneyIcon className="sidebarIcon" />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <StackedBarChartIcon className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
 
-         <div className="sidebarMenu">
-            <h3 className="sidebarTitle">Notifications</h3>
-            <ul className="sidebarList">
-               <li className="sidebarListItem">
-               <MailOutlineIcon className="sidebarIcon" />
-               Mail
-               </li>
-               <li className="sidebarListItem">
-               <DynamicFeedIcon className="sidebarIcon" />
-                Feedback
-               </li>
-               <li className="sidebarListItem">
-               <ChatBubbleOutlineIcon className="sidebarIcon" />
-                Messages
-               </li>
-            </ul>
-         </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <MailOutlineIcon className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeedIcon className="sidebarIcon" />
+              Feedback
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutlineIcon className="sidebarIcon" />
+              Messages
+            </li>
+          </ul>
+        </div>
 
-         <div className="sidebarMenu">
-            <h3 className="sidebarTitle">staff</h3>
-            <ul className="sidebarList">
-               <li className="sidebarListItem">
-               <PortraitIcon className="sidebarIcon" />
-               Manage
-               </li>
-               <li className="sidebarListItem">
-               <TimelineIcon className="sidebarIcon" />
-               Analytics
-               </li>
-               <li className="sidebarListItem">
-               <ReportIcon className="sidebarIcon" />
-               Reports
-               </li>
-            </ul>
-         </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <PortraitIcon className="sidebarIcon" />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <TimelineIcon className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <ReportIcon className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  )
-}
-export default Sidebar
+  );
+};
+export default Sidebar;
